a8e5166185cbf7b0791bb6e3e03d7c03878dfa99
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-02-14 01:39:10 +0600

    final release

--- js/script.js
+++ js/script.js
@@ -11,9 +11,27 @@ document.getElementById("btn-rectangle").addEventListener('click',function(){
 });
 
 document.getElementById("btn-parallelogram").addEventListener('click',function(){
-    const calcOfRectangleIs = elementById("parallelogram-b","parallelogram-h");
+    const calcOfParallelogramIs = elementById("parallelogram-b","parallelogram-h");
     document.getElementById("result-three").style.display = "inline-block";
-    document.getElementById("result-3").innerText = `${calcOfRectangleIs}`;
+    document.getElementById("result-3").innerText = `${calcOfParallelogramIs}`;
+});
+
+document.getElementById("btn-rhombus").addEventListener('click',function(){
+    const calcOfRhombusIs = elementById("rhombus-d1","rhombus-d2");
+    document.getElementById("result-four").style.display = "inline-block";
+    document.getElementById("result-4").innerText = `${calcOfRhombusIs}`;
+});
+
+document.getElementById("btn-pentagon").addEventListener('click',function(){
+    const calcOfPentagonIs = elementById("pentagon-p","pentagon-b");
+    document.getElementById("result-five").style.display = "inline-block";
+    document.getElementById("result-5").innerText = `${calcOfPentagonIs}`;
+});
+
+document.getElementById("btn-ellipse").addEventListener('click',function(){
+    const calcOfEllipseIs = Math.round(Math.PI * elementById("ellipse-a","ellipse-b"));
+    document.getElementById("result-six").style.display = "inline-block";
+    document.getElementById("result-6").innerText = `${calcOfEllipseIs}`;
 });
 
 function elementById(element1, element2){
